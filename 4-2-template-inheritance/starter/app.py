16c5963f5ffb4a5368de26fe9ed6ab624cae8357
Behnam Sepehri <behnam.sep@gmail.com>
2026-05-03 21:48:17 +0200

    set inheritance for the route

--- 4-2-template-inheritance/starter/app.py
+++ 4-2-template-inheritance/starter/app.py
@@ -1,16 +1,34 @@
+from datetime import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    user = {'username': 'Natasha'}
-    items = ['item 1', 'item 2', 'item 3']
-    return render_template('index.html', title='Home', user=user)
+    user = {"username": "Natasha"}
+    items = [
+        "bread",
+        "milk",
+        "eggs",
+        "coffee",
+        "apples",
+        "olive oil",
+    ]
+    return render_template(
+        "index.html",
+        title="Home",
+        user=user,
+        items=items,
+        year=datetime.now().year,
+    )
 
-@app.route('/welcome')
+
+@app.route("/welcome")
 def welcome():
-    return render_template('welcome.html')
+    return render_template("welcome.html", year=datetime.now().year)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
