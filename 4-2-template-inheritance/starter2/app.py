e87f3e091447956bda402fc1b11bdd229dd635d2
Behnam Sepehri <behnam.sep@gmail.com>
2026-05-03 22:35:47 +0200

    Refactor app.py for improved readability and update index.html to utilize Jinja blocks for dynamic content rendering.

--- 4-2-template-inheritance/starter2/app.py
+++ 4-2-template-inheritance/starter2/app.py
@@ -1,13 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
- return render_template('index.html', bb="behnam",cc="fff")
+    return render_template("index.html", bb="behnam", cc="fff")
 
 
-
-if __name__=='__main__':
-  app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(debug=True)
